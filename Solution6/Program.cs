0e101c3de2277c7da20a6d6799589d76cf19d098
Pass1948 <129037485+Pass1948@users.noreply.github.com>
2023-04-14 20:07:57 +0900

    feat : Up & Down 게임
    1. 컴퓨터는 0~999 중에 랜덤한 숫자를 뽑는다.
    2. 유저는 10번의 기회가 있다.
    3. 플레이어가 수를 입력 하면 컴퓨터는 그 수가 큰지, 작은지, 정답인지 알려준다.
    4. 10번의 기회 소진시 게임을 종료할껀지 재시작 할껀지 선택 할수 있다.

--- Solution6/Program.cs
+++ Solution6/Program.cs
@@ -4,7 +4,49 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("0~999사이에 숫자를 입력해주세요 : ");
+            Random random = new Random();
+            int randomNumber = random.Next(999);
+            int chance = 10;
+            while (chance > 0)
+            {
+                string text = Console.ReadLine();
+                int numder = int.Parse(text);
+                if (numder == randomNumber)
+                {
+                    Console.WriteLine("승리하셨습니다");
+                    break;
+                }
+                if (numder != randomNumber)
+                {
+                    chance--;
+                    Console.WriteLine("기회가 {0}번 남았습니다", chance);
+                    if (numder > randomNumber)
+                    {
+                        Console.WriteLine("{0}이 더 큽니다  ", numder);
+                        Console.WriteLine("다시 입력해주세요(0~999) : ");
+                    }
+                    else if (numder < randomNumber)
+                    {
+                        Console.WriteLine("{0}이 더 작습니다  ", numder);
+                        Console.WriteLine("다시 입력해주세요(0~999) : ");
+                    }
+                }
+                if (chance == 0)
+                {
+                    Console.WriteLine("다시 도전하시겠습니까? (1.예//2.아니요)");
+                    string text1 = Console.ReadLine();
+                    switch (text1)
+                    {
+                        case "1":
+                            chance = 10;
+                            break;
+                        case "2":
+                            Console.WriteLine("게임을 종료합니다");
+                            return;
+                    }
+                }
+            }
         }
     }
 }
