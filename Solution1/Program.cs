45807fd8fc9bb4344c2fb86cd51f258618cc66b9
Pass1948 <129037485+Pass1948@users.noreply.github.com>
2023-04-14 19:57:26 +0900

    feat : 문자열 속에 문자 찾기
    문제 풀이 코드 추가

--- Solution1/Program.cs
+++ Solution1/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Please enter a \"pineapple is yummy apple\"");
+            string text = Console.ReadLine();
+            text.Contains("apple");
+
+            if (text.Contains("apple") == true)
+            {
+                int b = text.IndexOf("apple");
+                Console.WriteLine(b);
+            }
+
         }
     }
 }
