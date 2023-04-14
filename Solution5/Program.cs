335117b7085491712bb920821f49e056976e9a12
Pass1948 <129037485+Pass1948@users.noreply.github.com>
2023-04-14 20:06:19 +0900

    feat : 배열 공통항목 찾는 솔루션
    정렬된 배열에서 공통항목을 찾는 함수 구현

--- Solution5/Program.cs
+++ Solution5/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] arr1 = { 1, 5, 5, 10 };
+            int[] arr2 = { 3, 4, 5, 5, 10 };
+            int[] arr3 = { 5, 5, 10, 20 };
+            foreach (int i in FindCommonItems(arr1, arr2, arr3))
+                Console.WriteLine(i);
+
+            static int[] FindCommonItems(int[] arr1, int[] arr2, int[] arr3)
+            {
+                List<int> result = new List<int>();
+                for (int i = 0; i < arr1.Length; i++)
+                    for (int i2 = 0; i2 < arr2.Length; i2++)
+                        for (int i3 = 0; i3 < arr3.Length; i3++)
+                            if (arr1[i] == arr2[i2] && arr1[i] == arr3[i3])
+                                if (!result.Contains(arr1[i]))
+                                    result.Add(arr1[i]);
+                return result.ToArray();
+            }
         }
     }
 }
