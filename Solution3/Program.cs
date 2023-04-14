09c7d2321c49fc25a99b3827c88e8aa07cfd70ec
Pass1948 <129037485+Pass1948@users.noreply.github.com>
2023-04-14 20:00:26 +0900

    feat : 소수판별 솔루션
    입력받은 숫자가 정수인지 소수인지 판별하는 함수 추가와 출력

--- Solution3/Program.cs
+++ Solution3/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            IsPrime(8);
         }
+        static bool IsPrime(int n)
+        {
+            int number = 2;
+            int resutl = number / n;
+            if (resutl == 0)
+            {
+                Console.WriteLine("정수입니다");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("소수입니다");
+                return false;
+            }
+        }
+
     }
 }
