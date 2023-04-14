51ba35400fdbc481d0358705801900a45df8709e
영신 이 <dldudtls100@naver.com>
2023-04-15 00:30:49 +0900

    fix : 슬라이드퍼즐 게임(수정)
    코드 수정중

--- Solution7/Program.cs
+++ Solution7/Program.cs
@@ -6,8 +6,8 @@ namespace Solution7
 {
     internal class Program
     {
-        enum Tile {None, number, player, keyBotten};
-        enum KeyMove {Up, Down, Left, Right, None }
+        enum Tile { None, number, player, keyBotten };
+        enum KeyMove { Up, Down, Left, Right, None }
 
         static void Main(string[] args)
         {
@@ -15,9 +15,12 @@ namespace Solution7
             while (true)
             {
                 KeyMove input = GameInput();
-                GameRender();                         
+                GameRender();
             }
         }
+
+
+
         static KeyMove GameInput()
         {
             KeyMove direction;
@@ -46,8 +49,15 @@ namespace Solution7
             }
             return direction;
         }
+        
+
+
         static void GameRender()
         {
+            int inputCount = 0;
+            bool congratulation = false;
+
+
             int[,] puzzleNum = new int[5, 5];
             int number = 1;
             for (int i = 0; i < 5; i++)
@@ -91,9 +101,136 @@ namespace Solution7
             }
 
 
+            while (true)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        if (puzzleNum[i, j] == 0)
+                        {
+                            Console.Write("0\t");
+                        }
+                        else
+                        {
+                            Console.Write(puzzleNum[i, j] + "\t");
+                        }
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine();
+                }
 
+                int checkNumber = 1;
+                int checkCount = 0;
+                if (!congratulation)
+                {
+                    for (int i = 0; i < 5; i++)
+                    {
+                        for (int j = 0; j < 5; j++)
+                        {
+                            if (puzzleNum[i, j] == checkNumber)
+                            {
+                                checkNumber++;
+                                checkCount++;
+                            }
+                        }
+                    }
+                    if (checkCount % 25 == 0)
+                    {
+                        congratulation = true;
+                        break;
+                    }
+                }
+                else
+                {
+                    break;
+                }
 
-
+                Console.WriteLine("현재 Input Count : " + inputCount);
+                Console.WriteLine("움직일 방향의 숫자를 입력하세요.");
+                Console.WriteLine("←(4), →(8), ↑(6), ↓(2)");
+                string input = Console.ReadLine();
+                int inputNum;
+                while (true)
+                {
+                    if (!int.TryParse(input, out inputNum) || (inputNum != 8 && inputNum != 6 && inputNum != 2 && inputNum != 4))
+                    {
+                        Console.WriteLine("다시 입력하세요. 이상한거 입력받았음");
+                        Console.WriteLine("←(4), →(8), ↑(6), ↓(2)");
+                        input = Console.ReadLine();
+                        Console.WriteLine();
+                        continue;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                if (inputNum == 4)
+                {
+                    for (int i = 0; i < 5; i++)
+                    {
+                        for (int j = 0; j < 5; j++)
+                        {
+                            if (puzzleNum[i, j] == 0 && j > 0)
+                            {
+                                int temp = puzzleNum[i, j];
+                                puzzleNum[i, j] = puzzleNum[i, j - 1];
+                                puzzleNum[i, j - 1] = temp;
+                            }
+                        }
+                    }
+                }
+                if (inputNum == 8)
+                {
+                    for (int i = 0; i < 5; i++)
+                    {
+                        for (int j = 0; j < 5; j++)
+                        {
+                            if (puzzleNum[i, j] == 0 && i > 0)
+                            {
+                                int temp = puzzleNum[i, j];
+                                puzzleNum[i, j] = puzzleNum[i - 1, j];
+                                puzzleNum[i - 1, j] = temp;
+                            }
+                        }
+                    }
+                }
+                if (inputNum == 6)
+                {
+                    for (int i = 0; i < 5; i++)
+                    {
+                        for (int j = 0; j < 5; j++)
+                        {
+                            if (puzzleNum[i, j] == 0 && j < 4)
+                            {
+                                int temp = puzzleNum[i, j];
+                                puzzleNum[i, j] = puzzleNum[i, j + 1];
+                                puzzleNum[i, j + 1] = temp;
+                                break;
+                            }
+                        }
+                    }
+                }
+                if (inputNum == 2)
+                {
+                    for (int i = 0; i < 5; i++)
+                    {
+                        for (int j = 0; j < 5; j++)
+                        {
+                            if (puzzleNum[i, j] == 0 && i < 4)
+                            {
+                                int temp = puzzleNum[i, j];
+                                puzzleNum[i, j] = puzzleNum[i + 1, j];
+                                puzzleNum[i + 1, j] = temp;
+                                break;
+                            }
+                        }
+                    }
+                }
+                Console.Clear();
+                inputCount++;
+            }
         }
     }
 }
